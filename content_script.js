d544e3624ff195b1a01ff74f738b385373d96838
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-08-25 22:11:06 +0200

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -2,10 +2,12 @@ var headerEnabled = false;
 var scriptEnabled = false;
 
 function clickElement() {
-  let a = document.querySelector("#q-620494849 > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.Mt\\(a\\).Px\\(4px\\)--s.Pos\\(r\\).Expand.H\\(--recs-card-height\\)--ml.Maw\\(--recs-card-width\\)--ml > div > div > div.Pos\\(a\\).B\\(0\\).Iso\\(i\\).W\\(100\\%\\).Start\\(0\\).End\\(0\\) > div > div.Mx\\(a\\).Fxs\\(0\\).Sq\\(70px\\).Sq\\(60px\\)--s.Bd.Bdrs\\(50\\%\\).Bdc\\(\\$c-ds-border-gamepad-like-default\\) > button");
+  let a = document.querySelector("#q1298270057 > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.Mt\\(a\\).Px\\(4px\\)--s.Pos\\(r\\).Expand.H\\(--recs-card-height\\)--ml.Maw\\(--recs-card-width\\)--ml > div.recsCardboard__cardsContainer.H\\(100\\%\\).Pos\\(r\\).Z\\(1\\) > div > div.Pos\\(a\\).B\\(0\\).Iso\\(i\\).W\\(100\\%\\).Start\\(0\\).End\\(0\\) > div > div.Mx\\(a\\).Fxs\\(0\\).Sq\\(70px\\).Sq\\(60px\\)--s.Bd.Bdrs\\(50\\%\\).Bdc\\(\\$c-ds-border-gamepad-like-default\\) > button")
   if (a) {
-    a.click();
-    console.log("Like clicked");
+    //a.click();
+    console.log('Like');
+  }else{
+    console.log('Error - Necesaria Actualización');
   }
 }
 
@@ -13,7 +15,7 @@ setInterval(function () {
   if (scriptEnabled) {
     clickElement();
   }
-}, 1000);
+}, 1500);
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.toggleHeader) {
