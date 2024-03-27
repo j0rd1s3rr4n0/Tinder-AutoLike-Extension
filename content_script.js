ecbc9df4fa0a524d8f126204436f4793c1f2f483
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2024-03-27 20:16:58 +0100

    Corregir función de clic en elemento en content_script.js

--- content_script.js
+++ content_script.js
@@ -49,10 +49,11 @@ setTimeout(() => {
 function clickElement() {
   let a = document.querySelector("#u-1035653303 > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.Mt\\(a\\).Px\\(4px\\)--s.Pos\\(r\\).Expand.H\\(--recs-card-height\\)--ml.Maw\\(--recs-card-width\\)--ml > div.recsCardboard__cardsContainer.H\\(100\\%\\).Pos\\(r\\).Z\\(1\\) > div > div.Pos\\(a\\).B\\(0\\).Iso\\(i\\).W\\(100\\%\\).Start\\(0\\).End\\(0\\) > div > div.Mx\\(a\\).Fxs\\(0\\).Sq\\(70px\\).Sq\\(60px\\)--s.Bd.Bdrs\\(50\\%\\).Bdc\\(\\$c-ds-border-gamepad-like-default\\) > button")
   if (a) {
-    console.log('Like');
+    //console.log('Like');
+    console.log(a);
     likes = likes + 1;
     sessionStorage.setItem("likes", likes);
-    if(prod){
+    if(prod == true){
       a.click();
     }
   }else{
